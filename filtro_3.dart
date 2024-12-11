483db922864681bb5bb7d40c9dffe606665858ca
Alexandre Sarraf <thiago-sarraf@hotmail.com>
2024-12-11 17:00:05 -0400

    Estudos de Dart.

--- filtro_3.dart
+++ filtro_3.dart
@@ -9,19 +9,21 @@ List<E> filtrar<E>(List<E> lista, bool Function(E) fn) {
 }
 
 main() {
-  var notas = [8.2, 7.3, 6.8, 5.4, 2.7, 9.3];
-  var boasNotasFn = (double nota) => nota >= 7.5;
+  var notas = [8.2, 7.3, 6.8, 5.4, 2.7, 9.3, 10];
+  var boasNotasFn = (num nota) => nota >= 7.5;
 
   //var somenteNotasBoas = filtrar<double>(notas, boasNotasFn);
   //print(somenteNotasBoas);
-  print(filtrar(notas, boasNotasFn));
 
-  var nomes = ['Ana', 'Bia', 'Rebeca', 'Gui', 'Joana'];
-  var nomesGrandesFn = (String nome) =>
-      nome.length >=
-      5; /*recebe nome tipo String,
-  retorna nome.length maior ou igual a 5*/
-  //var nomesGrandes = filtrar<String>(nomes, boasNotasFn);
-  //print(nomesGrandes);
-  print(filtrar(nomes, nomesGrandesFn));
+  print(filtrar<num>(notas, boasNotasFn));
+
+  var nomes = ['Ana', 'Bia', 'Rebeca', 'Guilherme', 'Joana'];
+  var nomesGrandesFn = (String nome) {
+    return nome.length >= 5;
+  }; /*recebe nome tipo String, retorna nome.length maior ou igual a 5*/
+
+  var nomesGrandes = filtrar<String>(nomes, boasNotasFn);
+  //print(filtrar(nomes, nomesGrandesFn));
+
+  print(nomesGrandes);
 }
