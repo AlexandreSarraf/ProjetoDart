38a6236839c60d5e7d0ef63381910c6a25abc1b5
Alexandre Sarraf <thiago-sarraf@hotmail.com>
2024-12-13 14:55:54 -0400

    Estudos de dart.

--- map_reduce.dart
+++ map_reduce.dart
@@ -1,7 +1,34 @@
 main() {
-  Map mapa = {};
-  /*bool Function(double) fn;*/
+  var alunos = [
+    {'nome': 'Alfredo', 'nota': 9.9},
+    {'nome': 'Wilson', 'nota': 9.3},
+    {'nome': 'Mariana', 'nota': 8.7},
+    {'nome': 'Guilherme', 'nota': 8.1},
+    {'nome': 'Ana', 'nota': 7.6},
+    {'nome': 'Ricardo', 'nota': 6.8},
+  ];
 
-  //criar class Pessoa
-  print(mapa);
+  //String Function(Map) pegarApenasONome = (aluno) => aluno['nome'];
+  String pegarApenasONome(Map aluno) {
+    return aluno['nome'];
+  }
+
+  var nomes = alunos.map(pegarApenasONome);
+
+  int qtdeDeLetras(String texto) {
+    return texto.length;
+  }
+
+  var letrasNum = nomes.map(qtdeDeLetras);
+
+  int Function(int) dobro = (numDobro) => numDobro * 2;
+  /*int dobro(int numDobro) {
+    return numDobro * 2;
+  }*/
+
+  var resultado = alunos.map(pegarApenasONome).map(qtdeDeLetras).map(dobro);
+
+  print(nomes);
+  print(letrasNum);
+  print(resultado);
 }
