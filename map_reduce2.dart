78fa7659adf0629d8c40beb3d225a52e74e43f92
Alexandre Sarraf <thiago-sarraf@hotmail.com>
2024-12-13 15:19:41 -0400

    Estudos de dart.

--- map_reduce2.dart
+++ map_reduce2.dart
@@ -1,4 +1,24 @@
-main() {
-  var notas = [7.3, 5.4, 7.7, 8.1, 5.5, 4.9, 9.1, 10.0];
-  var total = 0;
+void main() {
+  var nomes = ['Ana', 'João', 'Pedro'];
+  var alunos = [
+    {'nome': 'Alfredo', 'nota': 9.9},
+    {'nome': 'Wilson', 'nota': 9.3},
+    {'nome': 'Mariana', 'nota': 8.7},
+    {'nome': 'Guilherme', 'nota': 8.1},
+    {'nome': 'Ana', 'nota': 7.6},
+    {'nome': 'Ricardo', 'nota': 6.8},
+  ];
+
+  // Transformando os nomes para letras maiúsculas
+  var nomesEmMaiusculas = nomes.map((nome) => nome.toUpperCase());
+
+  //var alunosMaius = alunos.map((aluno) => aluno['nome'].toString());
+  //versão tradicional sem arrow na função.
+  var alunosMaius = alunos.map((aluno) {
+    return aluno['nome'].toString().toUpperCase();
+  });
+
+  print(nomesEmMaiusculas); // Saída: (ANA, JOÃO, PEDRO)
+  print(alunosMaius);
+  // Saída: (ALFREDO, WILSON, MARIANA, GUILHERME, ANA, RICARDO)
 }
