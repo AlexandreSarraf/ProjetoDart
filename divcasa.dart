1a944ad2480e8c86cfc585557db895aed21f257a
Alexandre Sarraf <thiago-sarraf@hotmail.com>
2025-01-06 16:22:44 -0400

    casa div

---
+++ divcasa.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+void main() {
+  // Solicita o valor total ao usuário
+  print("Informe o valor total:");
+  String? inputTotal = stdin.readLineSync();
+  double? total = double.tryParse(inputTotal ?? "");
+
+  if (total == null || total <= 0) {
+    print("Valor inválido! Certifique-se de informar um número positivo.");
+    return;
+  }
+
+  // Percentuais
+  double percentLiber = 0.06;
+  double percentAmor = 0.3134;
+  double percentTarcisio = 0.3133;
+  double percentThiago = 0.3133;
+  double percentMontanteThiago = 3.0;
+
+  // Cálculos
+  double valorLiber = (total * percentLiber);
+  double valorAmor = (total * percentAmor);
+  double valorTarcisio = (total * percentTarcisio);
+  double valorThiago = (total * percentThiago);
+
+  double montanteThiago = (valorThiago * percentMontanteThiago);
+
+  // Exibição dos resultados
+  print("Total: $total");
+  print("Líber (${percentLiber}%): $valorLiber");
+  print("Amor (${percentAmor}%): $valorAmor");
+  print("Tarcísio (${percentTarcisio}%): $valorTarcisio");
+  print("Thiago (${percentThiago}%): $valorThiago");
+  print("3% do montante de Thiago: $montanteThiago");
+}
