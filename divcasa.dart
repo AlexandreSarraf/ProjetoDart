9860e52751aa9d1dcf4df0a727b54fe84e30393c
Alexandre Sarraf <thiago-sarraf@hotmail.com>
2025-01-07 13:43:38 -0400

    divcasa.dart

--- divcasa.dart
+++ divcasa.dart
@@ -16,7 +16,7 @@ void main() {
   double percentAmor = 0.3134;
   double percentTarcisio = 0.3133;
   double percentThiago = 0.3133;
-  double percentMontanteThiago = 3.0;
+  double percentMontanteThiago = 3 / 100;
 
   // Cálculos
   double valorLiber = (total * percentLiber);
@@ -24,13 +24,14 @@ void main() {
   double valorTarcisio = (total * percentTarcisio);
   double valorThiago = (total * percentThiago);
 
-  double montanteThiago = (valorThiago * percentMontanteThiago);
+  double montanteThiago =
+      ((valorThiago - (valorThiago * 0.2)) * percentMontanteThiago);
 
   // Exibição dos resultados
   print("Total: $total");
-  print("Líber (${percentLiber}%): $valorLiber");
-  print("Amor (${percentAmor}%): $valorAmor");
-  print("Tarcísio (${percentTarcisio}%): $valorTarcisio");
-  print("Thiago (${percentThiago}%): $valorThiago");
-  print("3% do montante de Thiago: $montanteThiago");
+  print("Líber ($percentLiber%): $valorLiber");
+  print("Amor ($percentAmor%): $valorAmor");
+  print("Tarcísio ($percentTarcisio%): $valorTarcisio");
+  print("Thiago ($percentThiago%): $valorThiago");
+  print("3% do montante de Thiago($percentMontanteThiago%): $montanteThiago");
 }
