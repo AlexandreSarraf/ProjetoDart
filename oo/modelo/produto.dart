9f5866d3ce4f0bc24f38d8062c95f3d62ac1e664
Alexandre Sarraf <thiago-sarraf@hotmail.com>
2025-01-13 17:00:28 -0400

    Estudos de dart

--- oo/modelo/produto.dart
+++ oo/modelo/produto.dart
@@ -13,12 +13,20 @@ class Produto {
     return 0;
   }
 
-  void Mostrar() {
-    print("");
+  String ExibirAtributos() {
+    return '''
+    Produto:
+      Código: ${codigo ?? 'N/A'}
+      Nome: ${nome ?? 'N/A'}
+      Preço: ${preco != null ? 'R\$${preco!.toStringAsFixed(2)}' : 'N/A'}
+      Desconto: ${desconto != null ? '${(desconto! * 100).toStringAsFixed(1)}%' : 'N/A'}
+      Preço com Desconto: ${preco != null && desconto != null ? 'R\$${precoComDesconto!.toStringAsFixed(2)}' : 'N/A'}
+    ''';
   }
 }
 
 main() {
-  var produto1 = Produto(nome: "Notebook", preco: 3500, desconto: 0.1);
-  print(produto1.nome);
+  var produto1 =
+      Produto(codigo: 1, nome: "Notebook", preco: 3500, desconto: 0.1);
+  print(produto1.precoComDesconto);
 }
